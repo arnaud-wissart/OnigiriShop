18c11ef8a98759f9b786e946eee6f585ad88b7e4
Arnaud Wissart <arnaud.wissart@live.fr>
2025-07-20 20:30:09 +0200

    - déplacement des services business

--- src/OnigiriShop/Program.cs
+++ src/OnigiriShop/Program.cs
@@ -4,9 +4,9 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.Options;
 using OnigiriShop.Data;
 using OnigiriShop.Infrastructure;
-using OnigiriShop.Services;
 using Serilog;
 using FluentMigrator.Runner;
+using OnigiriShop.Services.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -37,39 +37,23 @@ builder.Services.AddOnigiriMigrations(connectionString);
 builder.Services.Configure<CalendarSettings>(builder.Configuration.GetSection("Calendar"));
 builder.Services.AddOnigiriDatabase(dbPath);
 builder.Services.AddOnigiriAuthentication();
-builder.Services.AddSingleton<ToastService>();
-builder.Services.AddSingleton<ErrorModalService>();
+
 builder.Services.AddScoped<SessionAuthenticationStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider, SessionAuthenticationStateProvider>();
-builder.Services.AddScoped<AuthService>();
 builder.Services.AddHttpContextAccessor();
-builder.Services.AddScoped<OrderService>();
 builder.Services.AddScoped<IMailjetClient>(sp =>
 {
     var config = sp.GetRequiredService<IOptions<MailjetConfig>>().Value;
     return new MailjetClient(config.ApiKey, config.ApiSecret);
 });
-builder.Services.AddScoped<EmailVariationService>();
-builder.Services.AddScoped<EmailTemplateService>();
-builder.Services.AddScoped<EmailService>();
-builder.Services.AddScoped<UserAccountService>();
-builder.Services.AddScoped<UserPreferenceService>();
-builder.Services.AddScoped<UserService>();
 builder.Services.AddSession();
-builder.Services.AddScoped<CartMergeService>();
-builder.Services.AddScoped<CartProvider>();
-builder.Services.AddSingleton<CartState>();
-builder.Services.AddScoped<AnonymousCartService>();
-builder.Services.AddScoped<CartService>();
-builder.Services.AddScoped<ProductService>();
-builder.Services.AddScoped<DeliveryCalendarService>();
-builder.Services.AddScoped<OrderExportService>();
-builder.Services.AddScoped<DeliveryService>();
 builder.Services.AddScoped(sp =>
 {
     var nav = sp.GetRequiredService<NavigationManager>();
     return new HttpClient { BaseAddress = new Uri(nav.BaseUri) };
 });
+
+builder.Services.AddBusinessServices();
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
@@ -94,11 +78,4 @@ app.MapFallbackToPage("/_Host");
 app.MapRazorPages();
 app.MapAuthEndpoints();
 
-ServiceProviderStatic = app.Services;
-
 app.Run();
-
-public partial class Program
-{
-    public static IServiceProvider ServiceProviderStatic { get; set; }
-}
--- src/OnigiriShop/Services/Extensions/BusinessServiceCollectionExtensions.cs
+++ src/OnigiriShop/Services/Extensions/BusinessServiceCollectionExtensions.cs
@@ -8,12 +8,20 @@ public static class BusinessServiceCollectionExtensions
         services.AddSingleton<ErrorModalService>();
         services.AddScoped<AuthService>();
         services.AddScoped<OrderService>();
+        services.AddScoped<EmailVariationService>();
+        services.AddScoped<EmailTemplateService>();
         services.AddScoped<EmailService>();
         services.AddScoped<UserAccountService>();
         services.AddScoped<UserPreferenceService>();
         services.AddScoped<UserService>();
+        services.AddScoped<CartMergeService>();
+        services.AddScoped<CartProvider>();
+        services.AddSingleton<CartState>();
+        services.AddScoped<AnonymousCartService>();
         services.AddScoped<CartService>();
         services.AddScoped<ProductService>();
+        services.AddScoped<DeliveryCalendarService>();
+        services.AddScoped<OrderExportService>();
         services.AddScoped<DeliveryService>();
         return services;
     }
